fc12951383fd0e56ef3fc9275de20480523ea2c9
Hitesh <hsodhi2003@gmail.com>
2023-10-15 20:38:32 +0530

    initial configuration

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,20 @@
+"""
+Storing information about the current state.
+Determining the valid moves.
+Keep a move log.
+"""
+
+
+class GameState():
+    def __init__(self):
+        # board is an 8x8
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
---
+++ Chess/ChessMain.py
@@ -0,0 +1,4 @@
+"""
+Main driver file.
+#Responsible for hadling user input and displaying the current GameState
+"""
