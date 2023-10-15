2a6f53b682d8777c02eb2d72d344ed9330c2b5ed
Hitesh <hsodhi2003@gmail.com>
2023-10-15 21:59:03 +0530

    make the chess board and pieces

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -1,34 +1,53 @@
 """
 Main driver file.
-#Responsible for hadling user input and displaying the current GameState
+# Responsible for handling user input and displaying the current GameState
 """
 import pygame as p
-from Chess import ChessEngine
-
+from Chess import ChessEngine  # Make sure to import the correct ChessEngine module
+MAX_FPS = 60
 WIDTH = HEIGHT = 512
 DIMENSION = 8
-SQ_SIZE = 15  #for animation later on
+SQ_SIZE = WIDTH // DIMENSION  # Adjusted to fit the board size
 IMAGES = {}
-
-"""
-initialise a global dictionary of images. 
-will be called only once
-"""
 
 def loadImages():
     pieces = ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bP', 'bR', 'bN', 'bB', 'bQ', 'bK']
     for piece in pieces:
-        IMAGES[pieces] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE,SQ_SIZE)) 
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
-"""
-The main driver for our code. This will handle user input and updating graphics
-"""
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r + c) % 2]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            piece = board[row][col]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
 def main():
     p.init()
-    screen = p.display.set_mode(WIDTH,HEIGHT)
+    screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
-    screen.fill(p.color("white"))
+    screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    print(gs.board)
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        clock.tick(MAX_FPS)
+        drawGameState(screen, gs)
+        p.display.flip()
 
-main()
+if __name__ == "__main__":
+    main()
