55d83c282300efe921686d9c3db4a6102d788ac6
Hitesh <hsodhi2003@gmail.com>
2023-10-15 20:41:52 +0530

    initial configuration

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -7,7 +7,10 @@ Keep a move log.
 
 class GameState():
     def __init__(self):
-        # board is an 8x8
+        # board is an 8x8 2d list, each element of the list has 2 characters.
+        # the first character represents the color of the piece 
+        # the secod represents the type of the piece.
+        # "--" represents blank space.
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
@@ -18,3 +21,5 @@ class GameState():
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        self.whiteToMove = True
+        self.moveLog = []
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -2,3 +2,33 @@
 Main driver file.
 #Responsible for hadling user input and displaying the current GameState
 """
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = 15  #for animation later on
+IMAGES = {}
+
+"""
+initialise a global dictionary of images. 
+will be called only once
+"""
+
+def loadImages():
+    pieces = ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bP', 'bR', 'bN', 'bB', 'bQ', 'bK']
+    for piece in pieces:
+        IMAGES[pieces] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE,SQ_SIZE)) 
+
+"""
+The main driver for our code. This will handle user input and updating graphics
+"""
+def main():
+    p.init()
+    screen = p.display.set_mode(WIDTH,HEIGHT)
+    clock = p.time.Clock()
+    screen.fill(p.color("white"))
+    gs = ChessEngine.GameState()
+    print(gs.board)
+
+main()
